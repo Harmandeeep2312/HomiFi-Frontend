a9b4e800aa063faf284fefe9d2522b6af4da0bec
Harmandeep2312 <harman145singh@gmail>
2025-09-10 18:59:29 +0530

    fixed errors

--- src/components/show.jsx
+++ src/components/show.jsx
@@ -13,6 +13,7 @@ import {
   Typography,
   TextField,
   Rating,
+  Toolbar
 } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
@@ -115,7 +116,7 @@ export default function Show() {
     <>
       <Navbar />
 
-      <div className="toolbar-offset"></div>
+      <Toolbar />
        <div className="main-content">
       <Box sx={{ width: "100%", py: 6, px: 2, pt: 10, background: "linear-gradient(to right, #6dd5ed, #2193b0)" }}>
         
